46feb8b91a6c1d5eade1da477daef9117db8e897
Mirdad1224 <ali.mirdad@yahoo.com>
2023-02-18 13:39:17 +0330

    modify the _app file and providing store and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.scss";
+import { store } from "../redux/store";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default MyApp;
