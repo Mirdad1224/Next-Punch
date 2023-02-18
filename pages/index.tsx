4844d749f87e71333b481e2eaeed334e22fd1483
Mirdad1224 <ali.mirdad@yahoo.com>
2023-02-18 13:40:04 +0330

    create main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Board } from "../components/Board";
+import { Header } from "../components/Header";
+
+const Home: NextPage = () => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
   return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
-}
+    <div>
+      <Head>
+        <title>2048 GAME</title>
+        <meta
+          name="description"
+          content="2048 Game made by Ali Mirdad"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container">
+        <Header />
+        <Board />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
